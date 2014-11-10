ba0773eac469268f816aebcc62d58b387f51e3b1
sofibenc <sofianesynchro@yahoo.fr>
2014-11-10 15:40:05 +0100

    Code indentation

--- forever/view/products/search_result.php
+++ forever/view/products/search_result.php
@@ -9,25 +9,25 @@
 		</div>
 	
 		<?php if (isset($body_content)) : ?>
-		<?php foreach ($body_content as $key): ?>
-		<?php 
-			$parent_cat="";
-			$page_link=($key['product_ref']==0)? "/forever/products/display/".$key['name'] : "/forever/products/display/".$key['parent_name']."/".$key['name'];
-			$img_link=($key['product_ref']==0) ? "src=\"/forever/img/categories/".$key["id"].".jpg\"" : "width=\"180\" height=\"180\" src=\"/forever/img/products/".$key["product_ref"].".jpg\"";
-		?>
-		<div id="desc_image_products">
-			<a title="<?php echo $key['title'];?>" href="<?php echo $page_link?>">
-				<div>
-					<img width="110" height="110"  <?php echo $img_link;?> alt="<?php echo $key['title']; ?>"/>
-					<div>
-						<?php echo $key['title']."<br><i>".$key['title_info'].", Ref : ".$key['product_ref']."</i>";?>
-					</div>
+			<?php foreach ($body_content as $key): ?>
+				<?php 
+					$parent_cat="";
+					$page_link=($key['product_ref']==0)? "/forever/products/display/".$key['name'] : "/forever/products/display/".$key['parent_name']."/".$key['name'];
+					$img_link=($key['product_ref']==0) ? "src=\"/forever/img/categories/".$key["id"].".jpg\"" : "width=\"180\" height=\"180\" src=\"/forever/img/products/".$key["product_ref"].".jpg\"";
+				?>
+				<div id="desc_image_products">
+					<a title="<?php echo $key['title'];?>" href="<?php echo $page_link?>">
+						<div>
+							<img width="110" height="110"  <?php echo $img_link;?> alt="<?php echo $key['title']; ?>"/>
+							<div>
+								<?php echo $key['title']."<br><i>".$key['title_info'].", Ref : ".$key['product_ref']."</i>";?>
+							</div>
+						</div>
+					</a>
 				</div>
-			</a>
-		</div>
-		<?php endforeach ?>
-		<?php else: ?>
-		0 produits trouvés
+			<?php endforeach ?>
+			<?php else: ?>
+			0 produits trouvés
 		<?php endif ?>
 	</div>
 </div>
